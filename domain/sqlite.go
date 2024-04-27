6fadea644376fe9b86c4510a5f8ac053aa715740
Og B. Maciel <omaciel@ogmaciel.com>
2024-04-27 19:30:32 -0400

    Downgraded gorm logging level to Error.

--- domain/sqlite.go
+++ domain/sqlite.go
@@ -19,7 +19,7 @@ func NewSqliteRepository() *SqliteRepository {
 	db, err := gorm.Open(
 		sqlite.Open("hockey.db"),
 		&gorm.Config{
-			Logger: logger.Default.LogMode(logger.Info),
+			Logger: logger.Default.LogMode(logger.Error),
 		})
 	if err != nil {
 		log.Fatal("Failed to connect to the database. \n", err)
